fffb5e540f543f8d949da70c26e2fdfb2df0cdf4
Shiyuan Gu <shiyuan.gu@geneticfinance.com>
2014-01-10 17:55:24 -0800

    myjava.el: change the default compile-command

---
+++ myjava.el
@@ -0,0 +1,14 @@
+ (require 'compile)
+ (add-hook 'java-mode-hook
+           (lambda ()
+	     (unless (file-exists-p "Makefile")
+	       (set (make-local-variable 'compile-command)
+                    ;; emulate make's .c.o implicit pattern rule, but with
+                    ;; different defaults for the CC, CPPFLAGS, and CFLAGS
+                    ;; variables:
+                    ;; $(CC) -c -o $@ $(CPPFLAGS) $(CFLAGS) $<
+		    (let ((file (file-name-nondirectory buffer-file-name)))
+                      (format "%s -g %s"
+                              "javac"
+			      file))))))
+(provide 'myjava)
