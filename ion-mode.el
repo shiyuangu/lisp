5ac945543162d98b96414ae468c86ed5b143300d
Shiyuan Gu <shiyuang@amazon.com>
2014-03-26 20:06:26 -0700

    Make Tab always indent four spaces.

--- ion-mode.el
+++ ion-mode.el
@@ -41,10 +41,45 @@
 		 `(,ion-keywords . font-lock-keyword-face))
    "Font lock for ion-mode")
 
+(defvar ion-mode-syntax-table
+  (let ((ion-mode-syntax-table (make-syntax-table)))
+	
+    ; This is added so entity names with underscores can be more easily parsed
+	(modify-syntax-entry ?- "w" ion-mode-syntax-table)
+	
+	; Comment styles are same as C++
+	(modify-syntax-entry ?/ ". 124b" ion-mode-syntax-table)
+	(modify-syntax-entry ?* ". 23" ion-mode-syntax-table)
+	(modify-syntax-entry ?\n "> b" ion-mode-syntax-table)
+	ion-mode-syntax-table)
+  "Syntax table for ion-mode")
+
+(defun ion-insert-tab (&optional arg)
+  "A wrapper for insert-tab to allow interactive call"
+  (interactive "P")
+  (insert-tab arg))
+
+(defvar ion-mode-map
+  (let ((ion-mode-map (make-keymap)))
+    (define-key ion-mode-map "\t" 'ion-insert-tab)
+    ion-mode-map)
+  "Keymap for ion major mode")
+
 ;;;###autoload
-(define-derived-mode ion-mode c++-mode "ION"
+(define-derived-mode ion-mode prog-mode "ION"
   "Major mode for editing ION files"
-  (set (make-local-variable 'font-lock-defaults) '(ion-font-lock-keywords)))
+  ;The follows codes will run before all hooks and parents' mode's hook
+  (message "running ion-mode code....")
+  (use-local-map ion-mode-map)
+  (set (make-local-variable 'font-lock-defaults) '(ion-font-lock-keywords))
+  (set-syntax-table ion-mode-syntax-table))
+
+(setq-default tab-width 4)
+(defun my-indent-setup()
+  ;(setq c-basic-offset 4)
+  (setq indent-tabs-mode nil)) ;;inserting tab is not allowed
+
+(add-hook 'ion-mode-hook 'my-indent-setup)
 
 (provide 'ion-mode)
 ;;; ion-mode.el ends here
